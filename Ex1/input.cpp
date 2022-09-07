cc088ab1aeb09cbd9ef87de59aa48a9338c1fe32
NelsonWang <wangnelson2@gmail.com>
2022-09-07 19:00:47 -0400

    example with input

---
+++ Ex1/input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int input_number;
+    cout<<"enter number";
+    cin>>input_number;
+    
+    cout<<"Entered number is: "<<input_number<<endl;
+}
