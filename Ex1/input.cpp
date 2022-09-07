e38bc7f0b4140bedfa5b367f66df649efa40ca31
NelsonWang <wangnelson2@gmail.com>
2022-09-07 19:09:35 -0400

    cpp

--- Ex1/input.cpp
+++ Ex1/input.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
 int main(){
     int input_number;
-    cout<<"enter number";
+    cout<<"enter number"<<endl;
     cin>>input_number;
-    
+    printf("%d is the number with some string: %s\n", input_number, "Some random string");
     cout<<"Entered number is: "<<input_number<<endl;
 }
